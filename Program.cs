a3c57946b874ae2bc0f59e8c4730beba05f68ef2
Mohammad <siddiqum@kean.edu>
2022-01-26 11:29:39 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string text = System.IO.File.ReadAllText("text.txt"); 
+Console.WriteLine(text);
+Console.WriteLine("Mohammad Raafi");
